23f8a16773911385ce44448474faecc031992d84
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-11-28 03:57:23 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,6 +22,6 @@ def now_serving(line)
   if line.length == 0
     puts "There is nobody waiting to be served!"
   else
-    puts "Currently serving #{line[0]}."
+    puts "Currently serving #{line}."
   end
 end
